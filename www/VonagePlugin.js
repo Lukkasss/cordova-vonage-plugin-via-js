38aca5bbd48a94c53d50773b006051356a467b56
Lucas Vilela <f.lucasgv@gmail.com>
2025-01-28 21:48:15 -0300

    Initial commit

---
+++ www/VonagePlugin.js
@@ -0,0 +1,45 @@
+var VonagePlugin = {
+    init: function (apiKey, sessionId, token, successCallback, errorCallback) {
+        // Initialize Vonage Video SDK
+        var session = OT.initSession(apiKey, sessionId);
+        session.connect(token, function (error) {
+            if (error) {
+                errorCallback(error);
+            } else {
+                successCallback();
+            }
+        });
+
+        // Store session for later use
+        this.session = session;
+    },
+
+    startPublishing: function (elementId, successCallback, errorCallback) {
+        if (!this.session) {
+            errorCallback("Session not initialized");
+            return;
+        }
+
+        // Create a publisher and attach it to the specified div
+        var publisher = OT.initPublisher(elementId, {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        }, function (error) {
+            if (error) {
+                errorCallback(error);
+            } else {
+                successCallback();
+            }
+        });
+
+        // Publish the stream
+        this.session.publish(publisher, function (error) {
+            if (error) {
+                errorCallback(error);
+            }
+        });
+    }
+};
+
+module.exports = VonagePlugin;
