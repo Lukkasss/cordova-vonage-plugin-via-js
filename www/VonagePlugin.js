61ccb7bcadc4e97eb4885e57a0b14d19b6735d32
Lucas Vilela <f.lucasgv@gmail.com>
2025-02-07 03:49:41 -0300

    Added iOS test

--- www/VonagePlugin.js
+++ www/VonagePlugin.js
@@ -42,15 +42,33 @@ var VonagePlugin = {
     },
 
     checkPermissions: function (successCallback, errorCallback) {
+        if (cordova.platformId === "android") {
+            this.checkAndroidPermissions(successCallback, errorCallback);
+        } else if (cordova.platformId === "ios") {
+            this.checkIOSPermissions(successCallback, errorCallback);
+        } else {
+            errorCallback("Unsupported platform");
+        }
+    },
+
+    checkAndroidPermissions: function (successCallback, errorCallback) {
         var permissions = cordova.plugins.permissions;
 
         // List of required permissions
         var requiredPermissions = [
             permissions.CAMERA,
             permissions.RECORD_AUDIO,
-            permissions.BLUETOOTH_CONNECT,
-            permissions.READ_PHONE_STATE
+            permissions.READ_PHONE_STATE,
+            permissions.MODIFY_AUDIO_SETTINGS,
+            permissions.INTERNET
         ];
+
+        // Add Bluetooth permission based on Android version
+        if (this.isAndroid12OrHigher()) {
+            requiredPermissions.push(permissions.BLUETOOTH_CONNECT);
+        } else {
+            requiredPermissions.push(permissions.BLUETOOTH);
+        }
 
         // Check if all required permissions are granted
         permissions.hasPermission(requiredPermissions, function (status) {
@@ -58,19 +76,45 @@ var VonagePlugin = {
                 // Request all required permissions
                 permissions.requestPermissions(requiredPermissions, function (requestStatus) {
                     if (requestStatus.hasPermission) {
-                        successCallback();
+                        successCallback("All required permissions granted");
                     } else {
-                        errorCallback("Required permissions denied");
+                        errorCallback("One or more permissions were denied");
                     }
                 }, function (error) {
                     errorCallback("Error requesting permissions: " + error);
                 });
             } else {
-                successCallback();
+                successCallback("All required permissions already granted");
             }
         }, function (error) {
             errorCallback("Error checking permissions: " + error);
         });
+    },
+
+    checkIOSPermissions: function (successCallback, errorCallback) {
+        // Use getUserMedia to request camera and microphone access
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+            .then(function (stream) {
+                // Stop the tracks to release the camera/microphone
+                stream.getTracks().forEach(track => track.stop());
+                successCallback("Camera and microphone permissions granted");
+            })
+            .catch(function (error) {
+                let message;
+                if (error.name === "NotAllowedError") {
+                    message = "Camera or microphone permission denied by user";
+                } else if (error.name === "NotFoundError") {
+                    message = "No media devices found";
+                } else {
+                    message = "An error occurred while requesting permissions: " + error.message;
+                }
+                errorCallback(message);
+            });
+    },
+
+    isAndroid12OrHigher: function () {
+        // Use the Cordova device plugin to get the Android version
+        return parseInt(device.version.split(".")[0]) >= 12;
     }
 };
 
