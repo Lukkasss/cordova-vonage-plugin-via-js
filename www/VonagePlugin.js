9c23b0d5085126adcb06964cf542ed7ff9d53d8f
Lucas Vilela <f.lucasgv@gmail.com>
2025-02-07 01:28:05 -0300

    Testing diagnostics

--- www/VonagePlugin.js
+++ www/VonagePlugin.js
@@ -39,6 +39,53 @@ var VonagePlugin = {
                 errorCallback(error);
             }
         });
+    },
+
+    checkPermissions: function (successCallback, errorCallback) {
+        // Check Camera Authorization
+        cordova.plugins.diagnostic.isCameraAuthorized(function (cameraAuthorized) {
+            if (!cameraAuthorized) {
+                // Request Camera Authorization
+                cordova.plugins.diagnostic.requestCameraAuthorization(function (cameraStatus) {
+                    if (cameraStatus === cordova.plugins.diagnostic.permissionStatus.GRANTED) {
+                        console.log("Camera permission granted");
+                        // Check Microphone Authorization
+                        VonagePlugin.checkMicrophonePermission(successCallback, errorCallback);
+                    } else {
+                        errorCallback("Camera permission denied");
+                    }
+                }, function (error) {
+                    errorCallback("Error requesting camera permission: " + error);
+                });
+            } else {
+                // Check Microphone Authorization
+                VonagePlugin.checkMicrophonePermission(successCallback, errorCallback);
+            }
+        }, function (error) {
+            errorCallback("Error checking camera permission: " + error);
+        });
+    },
+
+    checkMicrophonePermission: function (successCallback, errorCallback) {
+        cordova.plugins.diagnostic.isMicrophoneAuthorized(function (microphoneAuthorized) {
+            if (!microphoneAuthorized) {
+                // Request Microphone Authorization
+                cordova.plugins.diagnostic.requestMicrophoneAuthorization(function (microphoneStatus) {
+                    if (microphoneStatus === cordova.plugins.diagnostic.permissionStatus.GRANTED) {
+                        console.log("Microphone permission granted");
+                        successCallback();
+                    } else {
+                        errorCallback("Microphone permission denied");
+                    }
+                }, function (error) {
+                    errorCallback("Error requesting microphone permission: " + error);
+                });
+            } else {
+                successCallback();
+            }
+        }, function (error) {
+            errorCallback("Error checking microphone permission: " + error);
+        });
     }
 };
 
